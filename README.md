14da02b56966b168d88ba3b66e26d0a4092a4205
Adnan <adnan.aant@gmail.com>
2020-03-07 19:35:53 +0000

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Portfolio 1.0
+
+This is my first portfolio showcasing my projects
---
+++ index.js
@@ -0,0 +1,7 @@
+let width = window.innerWidth
+alert(`Current width is ${width}`)
+
+if (width !== window.innerWidth) {
+    alert(`Current width is ${width}`)
+        
+}
